4ae388f0fd48cb35eac4cbe0bab3ffb7130cbb49
Jose Aguilar <j.aguilar@berkeley.edu>
2025-11-07 14:50:25 +0000

    Created ligotools test folder and needed files

---
+++ ligotools/tests/test_readligo.py
@@ -0,0 +1,8 @@
+from ligotools import readligo
+
+def test_ligo_read():
+    assert loaddata("data/H-H1_LOSC_4_V2-1126259446-32.hdf5", 'H1') is not None
+
+def test_ligo_read_2():
+    assert loaddata("data/L-L1_LOSC_4_V2-1126259446-32.hdf5", 'L1') is not None
+
